5a37662806b366ffe28fafc09f3d60e33ab4ec83
zcj <canjiezeng@gmail.com>
2017-02-23 19:09:09 +0800

    画线

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.myapplication.view.MyView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public final int MODE_POINT=0;
+    public final int MODE_PATH=1;
+    private MyView myView;
+    private Button btnLine;
+    private int type=MODE_POINT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myView = (MyView) findViewById(R.id.myView);
+        btnLine = (Button) findViewById(R.id.btn_switch);
+        btnLine.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_switch:
+                if (type==MODE_PATH){
+                    type=MODE_POINT;
+                }else if (type==MODE_POINT){
+                    type=MODE_PATH;
+                }
+                myView.setMode(type);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/administrator/myapplication/view/MyView.java
+++ app/src/main/java/com/example/administrator/myapplication/view/MyView.java
@@ -3,6 +3,7 @@ package com.example.administrator.myapplication.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
@@ -16,12 +17,27 @@ import com.example.administrator.myapplication.R;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+
 public class MyView extends View {
+    public final int MODE_POINT=0;
+    public final int MODE_PATH=1;
     private Paint paint;
     private Path path;
-    private Point point;
     private ArrayList<Point> points;
+    private ArrayList<Path> paths;
+    private boolean isPoint=true;
+    private boolean isPath=false;
 
+
+    public void setMode(int mode){
+        if (mode==MODE_PATH){
+            isPath=true;
+            isPoint=false;
+        }else if(mode==MODE_POINT){
+            isPath=false;
+            isPoint=true;
+        }
+    }
 
     public MyView(Context context) {
         super(context);
@@ -41,23 +57,56 @@ public class MyView extends View {
     public void init(){
         paint=new Paint();
         paint.setColor(getResources().getColor(R.color.colorAccent));
+        paint.setStrokeWidth(10);
+        paint.setAntiAlias(true);
         points=new ArrayList<>();
+        paths=new ArrayList<>();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+        if (path!=null){
+            canvas.drawPath(path,paint);
+        }
         Iterator<Point> iterator = points.iterator();
         while (iterator.hasNext()){
             Point point = iterator.next();
             canvas.drawPoint(point.x,point.y,paint);
         }
+        Iterator<Path> pathIterator = paths.iterator();
+        while (pathIterator.hasNext()){
+            Path path = pathIterator.next();
+            canvas.drawPath(path,paint);
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        points.add(new Point((int)event.getX(),(int)event.getY()));
-        invalidate();
-        debug("onTouch");
+        if (isPoint) {
+            points.add(new Point((int) event.getX(), (int) event.getY()));
+            postInvalidate();
+            return true;
+        }
+        if (isPath){
+            float x = event.getX();
+            float y = event.getY();
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    path=new Path();
+                    path.moveTo(x,y);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    path.lineTo(x,y);
+                    postInvalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    paths.add(path);
+                    paint=null;
+                    break;
+            }
+            return true;
+        }
         return true;
     }
     private void debug(String log){
